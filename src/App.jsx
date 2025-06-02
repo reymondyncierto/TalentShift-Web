4d91670367ed64f129b5ab9f427a940fc93f3903
Reymond Yncierto <rpyncierto@up.edu.ph>
2025-06-02 21:28:13 +0800

    feat: ADD db initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+
 import ApplicantLandingPage from './pages/Application.page';
 import HRDashboard from './pages/HRDashboard.page';
+import { supabase } from './lib/Supabase.lib';
 
 export default function App() {
+
+  useEffect(() => {
+    async function initSupabase() {
+      try {
+        await supabase.auth.getSession();
+      } catch (error) {
+        console.error('Error initializing Supabase:', error);
+      }
+    }
+
+    initSupabase();
+  }, []);
+
   return (
     <Router>
       <Routes>
